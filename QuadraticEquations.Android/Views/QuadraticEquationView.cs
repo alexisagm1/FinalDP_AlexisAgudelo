a28cff05d7fcd8fc703f75fe754605294f1d4ba2
ALEXIS AGUDELO MAYA <alexisagudelo116893@correo.itm.edu.co>
2019-11-26 19:43:54 -0500

    create project Android

---
+++ QuadraticEquations.Android/Views/QuadraticEquationView.cs
@@ -0,0 +1,18 @@
+﻿
+using Android.App;
+using Android.OS;
+using MvvmCross.Platforms.Android.Views;
+using QuadraticEquations.Core.ViewModels;
+
+namespace QuadraticEquations.Android.Views
+{
+    [Activity(Label = "@string/app_name")]
+    public class QuadraticEquationView : MvxActivity<QuadraticEquationViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            this.SetContentView(Resource.Layout.QuadraticEquationPage);
+        }
+    }
+}
